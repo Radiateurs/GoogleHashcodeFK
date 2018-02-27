9f1e05f488936b9c8dad29c7b1989f4c56b3de7f
4rzael <maxime.agor23@gmail.com>
2018-02-27 15:36:49 +0000

    Added input function

---
+++ trial/input.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def read_input(filename):
+    def str_to_bool_array(s, true='T'):
+        return [bool(c == true) for c in s]
+
+    with open(filename, 'r') as file:
+        header = file.readline()
+        data = file.read()
+
+    ingredients = header.split()[2]
+    max_area = header.split()[3]
+    pizza = np.array([str_to_bool_array(s) for s in data.split()])
+    return ingredients, max_area, pizza
