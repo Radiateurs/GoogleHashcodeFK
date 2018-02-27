949a5a09343e1b61a7d6e36c5b39b3af1be3bde5
Jérémie Kalfon <jkobject@gmail.com>
2018-02-27 15:36:10 +0000

    myworkbench

---
+++ trial/divideandconquer.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def divide_and_conqueer(mat, L, H):
+
+
+def greedy(mat, L, H):
+    Lc = 0
+    Hc = 0
+    matc = np.zeros([1, 1], dtype=bool)
+    matc = mat[0, 0]
+    while Lc < L:
+        g
+        matc.
