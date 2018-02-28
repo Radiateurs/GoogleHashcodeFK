602c0ff04384f5d0e4f92d85b5746f014b0676b3
Jérémie Kalfon <jkobject@gmail.com>
2018-02-28 21:04:47 +0000

    adding my code

--- trial/recur.py
+++ trial/recur.py
@@ -11,32 +11,43 @@ def divide_and_conqueer(mat, L, H):
 
 
 def rec(mat, L, h):
-    lct = 0
-    lcm = 0
+    lt = 0
+    lm = 0
     h = 0
     mask = np.zeros(mat.shape)
-    b = np.array([0, 0])
-    e = np.array([0, 0])
-    e, lcm, lct = add(mat, e, np.array([1, 0]), h, lcm, lct)
+    deb = np.array([0, 0])
+    end = np.array([0, 0])
+    total = 0
+    while np.equal(deb, mat.shape):
+	    val, tot, deb, end = add(mat, mask, deb, end + np.array([0, 1]), lm, lt, l, h)
+	    if val:
+	    	mask[deb[0]:end[0], deb[1]:end[1]] = False
+	    	total += tot
+	    if deb[0] != mat.shape[0]:
+	   		deb += np.array([1, 0])
+	   	else :
+	   		deb = np.array([0,deb[1]+1])
+	return mask, total
 
 
-def add(mat, e, a, h, lcm, lct):
-    e += a
-    if e.sum() < h:
-        v = mat[e - a, e]
-        for i in matc:
-            print i
-            if i:
-                lct += 1
+
+def add(mat, mask, deb, end, lm, lt, l, h):
+	if mask[end]:
+		a = (end + np.array([1, 1])) - deb
+		tot = a[0] * a[1]
+	    if tot <= h:
+        	if i:
+                lt += 1
             else:
-                lcm += 1
-        if e[0] + 1 <= mat.shape[0]
-            e, lcm, lct = add(mat, e, np.array([0, 1]), h, lcm, lct)
-            if e and lcm > h and lct > h:
-                return e, lcm, lct
-
-        e, lcm, lct = add(mat, e, np.array([0, 1]), h, lcm, lct)
-        if e and lcm > h and lct > h:
-            return e, lcm, lct
-
-        return False, lcm, lct
+                lm += 1
+            mask[end]=False
+            if lt >=l and lm >=l:
+            	return True, tot, deb, end
+            else :
+            	val, tot, deb, end = add(mat, mask, deb, end+np.array([0,1]), lm, lt, l, h)
+            	if val: return True, tot, deb, end
+            	val, tot, deb, end = add(mat, mask, deb, end+np.array([1,0]), lm, lt, l, h)
+            	if val: return True, tot, deb, end
+    return False, None, None, None
+        
+  
