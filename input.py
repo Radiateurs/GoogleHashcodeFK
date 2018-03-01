e92cacd8d0b14ea5a6c0b7e6b90e830ac66a4293
4rzael <maxime.agor23@gmail.com>
2018-03-01 20:05:52 +0000

    Added ride index in input

--- input.py
+++ input.py
@@ -11,10 +11,11 @@ def read_inputs(filename):
         rides = int(header.split()[3])
         bonus = int(header.split()[4])
         steps = int(header.split()[5])
-        mat = np.zeros((rides, 3, 2))
+        mat = np.zeros((rides, 4, 2))
         for i in range(rides):
             line = file.readline()
             mat[i, 0, :] = (int(line.split()[0]), int(line.split()[1]))
             mat[i, 1, :] = (int(line.split()[2]), int(line.split()[3]))
             mat[i, 2, :] = (int(line.split()[4]), int(line.split()[5]))
+            mat[i, 3] = i
     return mat, rows, columns, vehicles, rides, bonus, steps
