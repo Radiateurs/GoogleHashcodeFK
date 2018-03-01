3444c546de6636a8cb817f787b1467e617bba07d
Jérémie Kalfon <jkobject@gmail.com>
2018-03-01 18:06:34 +0000

    working

--- input.py
+++ input.py
@@ -1,33 +1,20 @@
 import numpy as np
 
 
-def read_inut(filename):
-    def str_to_bool_array(s, true='T'):
-        return [bool(c == true) for c in s]
+def read_inputs(filename):
 
     with open(filename, 'r') as file:
         header = file.readline()
-        data = file.read()
-
-    ingredients = header.split()[2]
-    max_area = header.split()[3]
-    pizza = np.array([str_to_bool_array(s) for s in data.split()])
-    return int(ingredients), int(max_area), pizza
-
-
-def read_inputs(filename):
-	with open(filename, 'r') as file:
-        header = file.readline()
-        rows = header.split()[0]
-        columns = header.split()[1]
-        vehicles = header.split()[2]
-        rides = header.split()[3]
-        bonus = header.split()[4]
-        steps = header.split()[5]
+        rows = int(header.split()[0])
+        columns = int(header.split()[1])
+        vehicles = int(header.split()[2])
+        rides = int(header.split()[3])
+        bonus = int(header.split()[4])
+        steps = int(header.split()[5])
         mat = np.zeros((rides, 3, 2))
         for i in range(rides):
-        	line = file.readline()
-        	mat[i, 0, :] = line.split()[0:2]
-        	mat[i, 1, :] = line.split()[2:4]
-        	mat[i, 2, :] = line.split()[4:6]
+            line = file.readline()
+            mat[i, 0, :] = (int(line.split()[0]), int(line.split()[1]))
+            mat[i, 1, :] = (int(line.split()[2]), int(line.split()[3]))
+            mat[i, 2, :] = (int(line.split()[4]), int(line.split()[5]))
     return mat, rows, columns, vehicles, rides, bonus, steps
