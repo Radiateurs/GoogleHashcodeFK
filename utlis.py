2dadd4eb6d0ea3899cfeba9671d851881df74470
4rzael <maxime.agor23@gmail.com>
2018-03-01 19:36:06 +0000

    Added sort_rides

--- utlis.py
+++ utlis.py
@@ -5,3 +5,8 @@ def distances_list(vehicles, ride):
     for i, vehicle in enumeration(vehicles):
         distance[i] = (max(vehicle[0], ride[0]) - min(vehicle[0], ride[0])) + (max(vehicle[1], ride[1]) - min(vehicle[1], ride[1]))
     return distance
+
+def sort_rides(rides):
+    def sorter(ride):
+        return ride[2][0]
+    return np.array(sorted([r for r in rides], key=sorter))
