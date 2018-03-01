3d12e053c44dbc5c7e93253417a224bcfbf509e6
Pierre-Marie (Radiateurs) <pierre-marie.danieau@epitech.eu>
2018-03-01 20:01:53 +0000

    Nearest ride

--- utlis.py
+++ utlis.py
@@ -1,8 +1,20 @@
 import numpy as np
 
+def nearest_ride(vehicle, rides):
+    distance = np.zeros(vehicles.shape[0])
+    smallest = None
+    id = 0
+    for i, vehicle in enumerate(vehicles):
+        distance[i] = (max(vehicle[0], ride[0]) - min(vehicle[0], ride[0])) + (max(vehicle[1], ride[1]) - min(vehicle[1], ride[1]))
+    for i, dist in enumerate(distance):
+        if (smallest is None or smallest > dist)
+            smallest = dist
+            id = i
+    return id
+
 def distances_list(vehicles, ride):
     distance = np.zeros(vehicles.shape[0])
-    for i, vehicle in enumeration(vehicles):
+    for i, vehicle in enumerate(vehicles):
         distance[i] = (max(vehicle[0], ride[0]) - min(vehicle[0], ride[0])) + (max(vehicle[1], ride[1]) - min(vehicle[1], ride[1]))
     return distance
 
